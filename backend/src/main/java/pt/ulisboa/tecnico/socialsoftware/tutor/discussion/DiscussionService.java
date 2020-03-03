7df77e732070097512bf363e0f9780f426ab7f08
Pedro Pereira <pedro.l.pereira@tecnico.ulisboa.pt>
2020-03-03 20:29:45 +0000

    DDP tests defined.

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/DiscussionService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/DiscussionService.java
@@ -1,9 +1,10 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.discussion;
 
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.dto.StudentDto;
 
 public class DiscussionService {
-    public void createClarificationRequest(QuestionDto questionDto){
+    public void createClarificationRequest(ClarificationRequestDto clarificationRequestDto,Integer questionId, Integer studentId){
 
     }
 }
--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/service/createClarificationRequestServiceSpockTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/service/createClarificationRequestServiceSpockTest.groovy
@@ -8,28 +8,21 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto
 
 
 import pt.ulisboa.tecnico.socialsoftware.tutor.discussion.DiscussionService
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
 import spock.lang.Specification
 
 class createClarificationRequestServiceSpockTest extends Specification {
 
     static final String QUESTION_1_TITLE = "question1"
-    static final String QUESTION_KEY = 1
     static final String QUESTION_1_CONTENT = "o que é uma classe?"
+
+    static final String CLARIFICATION_TEXT = "Não percebi porque é que a opção correta é 1)."
 
     DiscussionService discussionService
 
-    def "the question exists and create Clarification Request" (){
+    def "the question exists, the student exists, the student answered the question and create Clarification Request" (){
         // creates a ClarificationRequest
-        given: "a question"
-        def question = new Question()
-        question.setTitle(QUESTION_1_TITLE)
-        question.setKey(QUESTION_KEY)
-        question.setContent(QUESTION_1_CONTENT)
-        question.setStatus(Question.Status.AVAILABLE)
-        and: "a questionDto"
-        def questionDto = new QuestionDto()
-        when:
-        def result = discussionService.createClarificationRequest(questionDto)
+        expect: false
     }
 
     def "the question does not exist" (){
@@ -37,7 +30,27 @@ class createClarificationRequestServiceSpockTest extends Specification {
         expect: false
     }
 
-    def "the question has not been answered by the logged in student" (){
+    def "the student is null" (){
+        //throws exception
+        expect: false
+    }
+
+    def "the question has not been answered by the student" (){
+        // throws exception
+        expect: false
+    }
+    
+    def "the clarification text is null" (){
+        //throws exception
+        expect: false
+    }
+
+    def "the clarification text is blank" (){
+        //throws exception
+        expect: false
+    }
+
+    def "there is a clarification already created for a certain question by a certain student" (){
         // throws exception
         expect: false
     }
