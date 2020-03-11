9df4216636c9936ce42de79ccb775b562b626a7c
Joao <brainsuprajr@gmail.com>
2020-03-11 16:53:59 +0000

    issue#7

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/dto/StudentQuestionDto.java
@@ -0,0 +1,41 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.dto;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.StudentQuestion;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
+
+import java.io.Serializable;
+
+public class StudentQuestionDto implements Serializable {
+
+    private Integer id;
+
+    private Question question;
+
+    private User user;
+
+    public StudentQuestionDto() {
+    }
+
+    public StudentQuestionDto(StudentQuestion studentQuestion) {
+
+        this.id = studentQuestion.getId();
+
+        this.question = studentQuestion.getQuestion();
+
+        this.user = studentQuestion.getUser();
+    }
+
+    public Integer getId() { return id; }
+
+    public void setId(Integer id) { this.id = id; }
+
+    public Question getQuestion() { return question; }
+
+    public void setQuestion(Question question) { this.question = question; }
+
+    public User getUser() { return user; }
+
+    public void setUser(User user) { this.user = user;}
+
+}
---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/repository/StudentQuestionRepository.java
@@ -0,0 +1,16 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.StudentQuestion;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public interface StudentQuestionRepository extends  JpaRepository<StudentQuestion, Integer> {
+
+}
