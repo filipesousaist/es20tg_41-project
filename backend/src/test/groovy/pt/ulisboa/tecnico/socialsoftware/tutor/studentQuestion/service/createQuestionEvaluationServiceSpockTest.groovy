afca8f5877a1464e3e701c96b1e7849b7d75a5e6
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2020-03-12 10:37:31 +0000

    Question Evaluation tests defined

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/studentQuestion/service/createQuestionEvaluationServiceSpockTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/studentQuestion/service/createQuestionEvaluationServiceSpockTest.groovy
@@ -1,20 +1,28 @@
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.auth.AuthService
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto
 import pt.ulisboa.tecnico.socialsoftware.tutor.studentQuestion.StudentQuestionService
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserService
 import spock.lang.Specification
 
 class CreateQuestionEvaluationServiceSpockTest extends Specification {
+    private static final String QUESTION_TITLE = "Question 1";
+    private static final String QUESTION_CONTENT = "What is the answer to this question?";
     @Autowired
-    def studentQuestionService
+    private StudentQuestionService studentQuestionService;
 
     def setup() {
-        studentQuestionService = new StudentQuestionService()
+
     }
 
     def "studentQuestion exists and justification is filled in"() {
         expect: false
     }
 
-    def "studentQuestion does not exist"() {
+    def "studentQuestion is null"() {
         expect: false
     }
 
@@ -28,5 +36,14 @@ class CreateQuestionEvaluationServiceSpockTest extends Specification {
 
     def "teacher does not teach the course of the question"() {
         expect: false
+    }
+
+    @TestConfiguration
+    static class StudentQuestionServiceImplTestContextConfiguration {
+
+        @Bean
+        StudentQuestionService studentQuestionService() {
+            return new StudentQuestionService()
+        }
     }
 }
