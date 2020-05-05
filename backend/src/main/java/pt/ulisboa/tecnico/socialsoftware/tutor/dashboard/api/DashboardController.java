9242b09f5bda7c4c64d24d907b2d9e1e69e48794
Pedro Pereira <pedro.l.pereira@tecnico.ulisboa.pt>
2020-05-05 18:47:55 +0100

    Dashboard do aluno (base) - implement webservice - closes #128

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/api/DashboardController.java
@@ -0,0 +1,24 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.DashboardService;
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.dto.DashboardDto;
+
+import java.util.List;
+
+@RestController
+public class DashboardController {
+
+    @Autowired
+    private DashboardService dashboardService;
+
+    @GetMapping("/executions/{executionId}/dashboard")
+    @PreAuthorize("hasRole('ROLE_STUDENT') and hasPermission(#executionId, 'COURSE.ACCESS')")
+    public List<DashboardDto> getDashboardStats(@PathVariable int executionId){
+        return this.dashboardService.getDashboardStats(executionId);
+    }
+}
